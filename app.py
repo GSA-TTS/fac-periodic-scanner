a628edfc3721f8d592de6a8b2b4e7f14e961fa9e
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2024-04-25 14:24:09 -0500

    default logging

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 # A very basic flask app to get the health check working for cgov and terraform. Due to this, the health check is
 # currently port based. We should change this in the future, however, for now, this is fine.
 
+import logging
+from logging.config import dictConfig
 import os
 from flask import Flask
 from threading import Thread
 from time import sleep
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+logger = logging.getLogger(__name__)
+
 def scan_loop():
     while True:
-        print('this is where I would scan a file')
+        logger.info('this is where I would scan a file')
         sleep(5)
 
 
@@ -22,5 +42,5 @@ def hello_world():
     return 'Temporary Flask App to ensure Terraform Apply Occurs'
 
 if __name__ == '__main__':
-    print("starting up...")
+    logger.info("starting up...")
     app.run(host='0.0.0.0', port=port)
